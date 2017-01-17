1f96e100cd73d7b0343026fea15b49e44b29db40
zhaozhe <zhaozhe0831@vip.qq.com>
2017-01-17 12:01:54 +0800

    对 webpack 配置 进行修改

--- build/build.js
+++ build/build.js
@@ -21,16 +21,18 @@ spinner.start()
 var assetsPath = path.join(config.build.assetsRoot, config.build.assetsSubDirectory)
 rm('-rf', assetsPath)
 mkdir('-p', assetsPath)
-cp('-R', 'static/*', assetsPath)
+cp('-R', 'src/components/*', assetsPath)
 
-webpack(webpackConfig, function(err, stats) {
-    spinner.stop()
-    if (err) throw err
-    process.stdout.write(stats.toString({
-        colors: true,
-        modules: false,
-        children: false,
-        chunks: false,
-        chunkModules: false
-    }) + '\n')
-})
+spinner.stop()
+
+// webpack(webpackConfig, function(err, stats) {
+//     spinner.stop()
+//     if (err) throw err
+//     process.stdout.write(stats.toString({
+//         colors: true,
+//         modules: false,
+//         children: false,
+//         chunks: false,
+//         chunkModules: false
+//     }) + '\n')
+// })
